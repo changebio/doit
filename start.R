3ae49cd88ce2c06b69cfff8f99dff0909b5b0627
changebio <changebio@users.noreply.github.com>
2015-12-14 17:40:07 +0800

    delet useless info

--- start.R
+++ start.R
@@ -1,13 +1,11 @@
 #start
 getwd()
-<<<<<<< HEAD
 #first of all
 setwd("/mnt/local-disk1/rsgeno2/huangyin")
 # create a vector
 a=rep(1,10)
 # sum
 sum(a)
-=======
+
 #first
 setwd("/mnt/local-disk1/rsgeno2/huangyin")
->>>>>>> cf2ba02645d54af255d1e7a2430cbab04567f96c
